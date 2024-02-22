e69b8a056981a46a185fd8f05edf8ad325ed7cdf
Brianoyaro <brianoyaro2000@yahoo.com>
2024-02-22 16:58:15 +0300

    handled 3

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""api app module"""
+from flask import Flask
+from os import getenve
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+
+@app.teardown_appcontext
+def close():
+    """closes the storad engine"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+
