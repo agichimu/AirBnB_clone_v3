f1f70eefeb91b37231d409031bdd730881e8071f
Brianoyaro <brianoyaro2000@yahoo.com>
2024-02-22 18:17:12 +0300

    handled 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """api app module"""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from os import getenv
 from models import storage
 from api.v1.views import app_views
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def close(self):
     """closes the storad engine"""
     storage.close()
+
+@app.errorhandler(404)
+def error_handler(e):
+    """handles 404 errors"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
