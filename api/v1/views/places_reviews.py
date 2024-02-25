2afb72043b71c837b14d97d0141a0750bc33d02e
Brianoyaro <134777190+Brianoyaro@users.noreply.github.com>
2024-02-25 15:55:15 +0300

    Update places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -5,12 +5,15 @@ from flask import jsonify, abort, request
 from models import storage
 from models.review import Review
 from models.place import Place
+from models.user import User
 
 
 @app_views.route("/places/<place_id>/reviews", methods=['GET'])
 def get_reviews(place_id):
     """Retrieves the list of all reviews in a place object"""
     all_places = storage.get('Place', place_id)
+    if all_places is None:
+        abort(404)
     values = [val.to_dict() for val in all_places.reviews]
     return jsonify(values)
 
@@ -37,8 +40,14 @@ def post_review(place_id):
     if not request.get_json():
         return jsonify({"error: Not a JSON"}), 400
     blob = request.get_json()
-    if 'name' not in blob:
-        return jsonify({"error: Missing name"}), 400
+    if 'user_id' not in blob:
+        return jsonify({"error: Missing user_id"}), 400
+    user_id = blob['user_id']
+    users = storage.get('User', user_id)
+    if users is None:
+        abort(404)
+    if 'text' not in blob:
+        return jsonify({"error: Missing text"}), 400
     """create it using the blob data"""
     newObject = Review(**blob)
     """save it to persist in memory"""
@@ -57,9 +66,11 @@ def put_review(review_id):
     val = storage.get('Review', review_id)
     if val is None:
         abort(404)
-    """because our api deals __class__,id, name, created_at,updated_at and we are to
-    Ignore keys: id, created_at and updated_at"""
-    val['name'] = blob['name']
+    for key, value in blob.items():
+        if key in ['id', 'user_id', 'place_id', 'created_at', 'updated_at']:
+            pass
+        else:
+            setattr(val, key, value)
     """to persist in storage"""
     val.save()
     return jsonify(val.to_dict())
